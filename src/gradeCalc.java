093d061a0e5551998302d3a8561c0d63d7e776f0
ami-rayed-na <rayed.razzak36@gmail.com>
2026-02-28 13:34:22 +0600

    Update

--- src/gradeCalc.java
+++ src/gradeCalc.java
@@ -1,37 +1,22 @@
 public class gradeCalc {
-    public static void grade(int a){
-        if(a<40) {
-            System.out.println("F");
-            return;
-        }
-        else if (a>=40 && a<60){
-            System.out.println("C+");
-            return;
-        }
-        else if(a>=60 && a<70){
-            System.out.println("B");
-            return;
-        }
-        else if(a>=70 && a<80){
-            System.out.println("A-");
-            return;            
-        }
-        else if(a>=80 && a<90){
-            System.out.println("A");
-            return;            
-        }
-        else if(a>=90 && a<=100){
-            System.out.println("A+");
-            return;            
-        }
+    public static String grade(int a){
+        if(a<40) return "F";
 
-        else {
-            System.out.println("Invalid number");
-            return;
-        }
+        else if (a<60) return "C";
+
+        else if(a<70) return "B";
+
+        else if(a<80) return "A-";     
+
+        else if(a<90) return "A";       
+
+        else if(a<=100) return "A+";            
+
+        else return "Invalid number";
     }
 
     public static void main (String[] args){
+        
         System.out.println("You got " + grade(39));
     }
 }
