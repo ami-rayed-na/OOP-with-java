635fc65685e763af49e15ec84347eae19b1512ed
ami-rayed-na <rayed.razzak36@gmail.com>
2026-03-07 16:22:44 +0600

    series printing

--- src/exe.java
+++ src/exe.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 public class exe {
     public static void main(String[] args){
-        Scanner scan= new Scanner(System.in);
-        System.out.println("Enter the year");
-        int year = scan.nextInt();
+        Scanner number = new Scanner(System.in);
+        System.out.print("Last step of the series:");
+        int limit= number.nextInt();
 
-        new LeapYear(year);
+        new SimpleSeries();
 
-        scan.close();
+        SimpleSeries.Odd.oddSeriesFor(limit);
+        System.out.println();
+        SimpleSeries.Odd.oddSeriesWhile(limit);
+        System.out.println();
+        SimpleSeries.Odd.oddSeriesDoWhile(limit);
+        System.out.println();
+
+        SimpleSeries.Even.evenSeriesFor(limit);
+        System.out.println();
+        SimpleSeries.Even.evenSeriesWhile(limit);
+        System.out.println();
+        SimpleSeries.Even.evenSeriesDoWhile(limit);
         
+        number.close();        
     }
     
 }
