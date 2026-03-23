90ececc16bb45201a540e51f021888b01d91bbbd
ami-rayed-na <rayed.razzak36@gmail.com>
2026-03-23 21:02:25 +0600

    pblm 4

---
+++ src/Pattern.java
@@ -0,0 +1,10 @@
+public class Pattern{
+    public static void patternPrint(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/exe.java
+++ src/exe.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class exe {
     public static void main(String[] args){
-       new ReverseArr();
-    }
+    Scanner num= new Scanner(System.in); 
+    System.out.println("Enter the value of n:");
+    int n= num.nextInt();
+
+    Pattern.patternPrint(n);
     
+    num.close();
+    }    
 }
