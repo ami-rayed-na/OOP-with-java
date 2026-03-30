4d120d6f90590fec281d5aab198ff55f36a9473d
ami-rayed-na <rayed.razzak36@gmail.com>
2026-03-30 22:08:30 +0600

    wallet task 4

--- src/exe.java
+++ src/exe.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class exe {
     public static void main(String[] args){
     Scanner ptr = new Scanner(System.in); 
-    Wallet3 obj1 = new Wallet3(500); 
+    Wallet4 obj1 = new Wallet4(500); 
     
     System.out.print("Enter the amount you want to deposit :");
     double amount= ptr.nextDouble();
@@ -12,7 +12,7 @@ public class exe {
     double w_amount= ptr.nextDouble();
     obj1.withdraw(w_amount);
 
-    Wallet3 obj2 = new Wallet3(300); 
+    Wallet4 obj2 = new Wallet4(300); 
     
     System.out.print("Enter the amount you want to deposit :");
     double amount1= ptr.nextDouble();
@@ -20,7 +20,9 @@ public class exe {
 
     System.out.print("Enter the amount you want to withdraw :");
     double w_amount1= ptr.nextDouble();
-    obj2.withdraw(w_amount1);
+    System.out.print("Please provide your withdraw method: ");
+    String method= ptr.next();
+    obj2.withdraw(w_amount1,method);
 
     obj1.printInfo();
     obj2.printInfo();
